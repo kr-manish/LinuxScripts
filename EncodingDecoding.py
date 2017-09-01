0ed5e8d676241339589a07e606afdc0159ef94d8
kr-manish <kumar8manish@gmail.com>
2017-09-01 18:55:48 +0530

    base64 decode added

--- EncodingDecoding.py
+++ EncodingDecoding.py
@@ -4,21 +4,25 @@ import os
 import sys
 
 import argparse
+import base64
 
-def base64_encode_decode(value, action):
-
+def base64_encode_decode(data, action):
+        if action == 'decode':
+                decoded = base64.b64decode(data)
+                print decoded
 
 def main():
         parser = argparse.ArgumentParser()
-        parser.add_argument("type", choices=['base64', 'caeser', 'vigenere', 'DES', 'RSA'], help="which type of encryption/decryption to be used.")
-        group = parser.add_mutually_exclusive_group()
+        parser.add_argument("method", choices=['base64', 'caeser', 'vigenere', 'DES', 'RSA'], help="which type of encryption/decryption to be used.")
+        group = parser.add_mutually_exclusive_group(required=True)
         group.add_argument("--encode", help="For encoding a string")
         group.add_argument("--decode", help="For decoding")
-        parser.add_argument("data", help="Data to encrypt/decrypt")
         args = parser.parse_args()
 
-        if args.type == 'base64':
-                print "base64"
+        if args.method == 'base64':
+                action = 'encode' if args.encode else 'decode'
+                data = args.encode if action=='encode' else args.decode
+                base64_encode_decode(data, action)
 
 if __name__ == "__main__":
         main()
